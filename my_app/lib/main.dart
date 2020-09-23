4f21eb687cc0035aebca5abc35265cb23d040cce
Takahiro Suzuki <t.suzuki@techd.jp>
2020-09-23 22:57:10 +0900

    02 Flutter: Stateless Widgets

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,22 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-      new MaterialApp(
-        title: 'Hello World App',
-        home: myApp(),
-      )
-  );
+  runApp(new MaterialApp(
+    title: 'Hello World App',
+    home: MyStatelessWidget(),
+  ));
 }
 
-class myApp extends StatelessWidget {
+class MyStatelessWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Hello World App")),
-      body: Center(
-        child: Text("Hello, World")
+      appBar: AppBar(title: Text("Stateless Widget")),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            MyCard(
+                title: Text("I love Flutter", style: TextStyle(fontSize: 20.0)),
+                icon: Icon(Icons.favorite, size: 40.0, color: Colors.redAccent)
+            ),
+            MyCard(
+                title: Text("I love Donuts", style: TextStyle(fontSize: 20.0)),
+                icon: Icon(Icons.favorite, size: 40.0, color: Colors.brown)
+            ),
+            MyCard(
+                title: Text("I see you", style: TextStyle(fontSize: 20.0)),
+                icon: Icon(Icons.visibility, size: 40.0, color: Colors.blue)
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class MyCard extends StatelessWidget {
+  MyCard({this.title, this.icon});
+
+  final Widget title;
+  final Widget icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.only(bottom: 20.0),
+        child: Card(
+            child: Container(
+                padding: EdgeInsets.all(15.0),
+                child: Column(children: <Widget>[this.title, this.icon]))));
   }
 }
